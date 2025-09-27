2f650a962e77cda97cb2ae4a793476f7e4a2df28
João Vitor Dias <joaovitordias002@gmail.com>
2025-09-27 14:46:58 -0300

    refactor: Reestruturação do diretório do projeto
    Ajustes de pastas para dentro de app e src. Ajuste de configurações para arquivos e importações relacionadas.

--- app/src/routes.py
+++ app/src/routes.py
@@ -1,23 +1,22 @@
-from projeto import app
-from flask import render_template, url_for
+from flask import render_template, url_for, Blueprint
 
+bp = Blueprint('routes', __name__)
 
-
-@app.route('/')
+@bp.route('/')
 def homepage():
     return render_template("index.html")
 
-@app.route('/conteudo')
+@bp.route('/conteudo')
 def conteudo():
     return render_template('conteudo.html') 
 
 
-@app.route('/conteudo/modulo1')
+@bp.route('/conteudo/modulo1')
 def modulo1():
     return render_template('modulo1.html')
 
 
-@app.route('/conteudo/modulo1/modulo1s2')
+@bp.route('/conteudo/modulo1/modulo1s2')
 def modulo1s2():
     return render_template('modulo1s2.html')
 
